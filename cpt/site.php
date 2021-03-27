d4e4ea16edcf8a62ac3fe268194fb286eb7cad31
Oz Ramos <pixelfelt@gmail.com>
2021-03-27 10:47:58 -0700

    #2 [site-visit-links]

--- cpt/site.php
+++ cpt/site.php
@@ -7,3 +7,24 @@ add_shortcode('site-bookmark-last-update', function () {
   global $post;
   return get_the_modified_date();
 });
+
+/**
+ * Lists links to visit
+ */
+add_shortcode('site-visit-links', function () {
+  global $post;
+
+  $links = get_field('site_links_to_try');
+  
+  ob_start(); ?>
+
+    <ul class="site-visit-links">
+      <?php foreach ($links as $link): ?>
+        <li>
+          <a href="<?= $link['site_link'] ?>"><?= $link['site_link'] ?></a>
+        </li>
+      <?php endforeach ?>
+    </ul>
+    
+  <?php return ob_get_clean();
+});
