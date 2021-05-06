7e855eb0851090eaff15fea65a6dec70251b5b86
Oz Ramos <pixelfelt@gmail.com>
2021-05-06 12:58:56 -0700

    #3 Adds handsfree styles to bookmark button

--- shortcodes/pixelfelt.php
+++ shortcodes/pixelfelt.php
@@ -8,8 +8,15 @@ add_shortcode('pixelfelt-bookmarklet', function () {
 
   ob_start(); ?>
     <div class="w-btn-wrapper width_auto align_left">
-      <a class="w-btn us-btn-style_1" href="javascript:<?= esc_attr($js) ?>">
+      <a class="w-btn us-btn-style_1 handsfree-show-when-stopped" href="javascript:<?= esc_attr($js) ?>">
         <span class="w-btn-label">Pixelfelt 🖐👀🖐</span>
+      </a>
+      <a class="w-btn us-btn-style_1 handsfree-show-when-loading" href="javascript:<?= esc_attr($js) ?>">
+        <i class="fas fa-circle-notch fa-spin"></i>
+        <span class="w-btn-label">Loading</span>
+      </a>
+      <a class="w-btn us-btn-style_1 active handsfree-show-when-started">
+        <span class="w-btn-label">Stop Pixelfelt</span>
       </a>
     </div>
   <?php return ob_get_clean();
